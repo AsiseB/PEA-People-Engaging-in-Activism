4b87d6cec78dbfbf987a6fdc9c01c834ebfc5ebf
asise bhinder <asisebhinder@gmail.com>
2022-07-21 11:57:16 -0700

    adults page edited

--- PEA/AdultsViewController.swift
+++ PEA/AdultsViewController.swift
@@ -9,13 +9,76 @@ import UIKit
 
 class AdultsViewController: UIViewController {
 
+    @IBOutlet weak var HandsOn: UIButton!
+    
+    @IBOutlet weak var PDX: UIButton!
+    
+    @IBOutlet weak var DSP: UIButton!
+    
+    @IBOutlet weak var TP: UIButton!
+    
+    @IBOutlet weak var BRO: UIButton!
+    
+    @IBOutlet weak var SEI: UIButton!
+    
+    @IBOutlet weak var RegisterToVote: UIButton!
+    
+    @IBOutlet weak var SampleBallot: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
     
-
+    @IBAction func HandsOn(_ sender: Any) {
+        let url = URL (string:
+        "https://www.handsonportland.org")!
+        UIApplication.shared.open (url)
+    }
+    
+    @IBAction func PDX(_ sender: Any) {
+        let url = URL (string:
+        "https://350pdx.org/action/")!
+        UIApplication.shared.open (url)
+    }
+    
+    @IBAction func DSP(_ sender: Any) {
+        let url = URL (string:
+        "https://www.dontshootpdx.org/tag/volunteer/")!
+        UIApplication.shared.open (url)
+    }
+    
+    @IBAction func TP(_ sender: Any) {
+        let url = URL (string:
+        "https://www.tprojects.org/how-help-volunteer/volunteer")!
+        UIApplication.shared.open (url)
+    }
+    
+    @IBAction func BRO(_ sender: Any) {
+        let url = URL (string:
+        "https://www.basicrights.org/get-involved/volunteer/")!
+        UIApplication.shared.open (url)
+    }
+    
+    @IBAction func SEI(_ sender: Any) {
+        let url = URL (string:
+        "https://www.selfenhancement.org")!
+        UIApplication.shared.open (url)
+    }
+    
+    @IBAction func RegisterToVote(_ sender: Any) {
+        let url = URL (string:
+        "https://sos.oregon.gov/voting/Pages/current-election.aspx")!
+        UIApplication.shared.open (url)
+    }
+    
+    @IBAction func SampleBallot(_ sender: Any) {
+        let url = URL (string:
+        "https://ballotpedia.org/Oregon_elections,_2022")!
+        UIApplication.shared.open (url)
+    }
+    
     /*
     // MARK: - Navigation
 
