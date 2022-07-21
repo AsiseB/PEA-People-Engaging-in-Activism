c9ce80f6c4272892c46905caaad96482b48a2f61
katherine-may <katdanm@gmail.com>
2022-07-21 09:52:56 -0700

    add color and buttons to under 14 page

--- PEA/KidsViewController.swift
+++ PEA/KidsViewController.swift
@@ -9,13 +9,53 @@ import UIKit
 
 class KidsViewController: UIViewController {
 
+    @IBOutlet weak var foodDriveButton: UIButton!
+    
+    @IBOutlet weak var donateBooksButton: UIButton!
+    
+    @IBOutlet weak var catToyButton: UIButton!
+    
+    @IBOutlet weak var plantTreeButton: UIButton!
+    
+    
+    @IBOutlet weak var sortFoodButton: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
     
-
+    
+    @IBAction func foodDriveButton(_ sender: Any) {
+        let url = URL (string: "https://urbangleaners.org/give-food/")!
+                UIApplication.shared.open(url)
+    }
+    
+    
+    @IBAction func donateBooksButton(_ sender: Any) {
+        let url = URL (string: "https://www.childrensbookbank.org/give-books-23/")!
+               UIApplication.shared.open(url)
+    }
+    
+    @IBAction func catToyButton(_ sender: Any) {
+        let url = URL(string: "https://www.handsonportland.org/opportunity/a0CA000000Oorx7/diy-toys-for-cats?id=a0CA000000Oorx7&layoutViewMode=tablet")!
+               UIApplication.shared.open(url)
+    }
+    
+    @IBAction func plantTreeButton(_ sender: Any) {
+        let url = URL(string: "https://friendsoftrees.org/event-calendar/")!
+               UIApplication.shared.open(url)
+        
+    }
+    
+    @IBAction func sortFoodButton(_ sender: Any) {
+        let url = URL(string: "https://www.handsonportland.org/opportunity/a0C1200000pw50JEAQ/stock-the-cupboard-oregon-food-bank-age-6-hands-on?id=a0C1200000pw50JEAQ&layoutViewMode=tablet")!
+                UIApplication.shared.open(url)
+        
+    }
+    
     /*
     // MARK: - Navigation
 
