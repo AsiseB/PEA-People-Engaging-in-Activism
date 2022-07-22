ae55d5ed92b56f1fcfd0cc5b353c7e95989c4e96
katherine-may <katdanm@gmail.com>
2022-07-21 20:24:06 -0700

    finished current events page

--- PEA/CurrentEventsViewController.swift
+++ PEA/CurrentEventsViewController.swift
@@ -10,11 +10,47 @@ import UIKit
 class CurrentEventsViewController: UIViewController {
     
 
+    @IBOutlet weak var helpUkraineButton: UIButton!
+    
+    @IBOutlet weak var inAreaButton: UIButton!
+    
+    @IBOutlet weak var myLawsButton: UIButton!
+    
+    @IBOutlet weak var supportPlannedButton: UIButton!
+    
+    @IBOutlet weak var otherOptionsButton: UIButton!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func helpUkraineButton(_ sender: Any) {
+        let url = URL (string: "https://www.npr.org/2022/02/25/1082992947/ukraine-support-help")!
+        UIApplication.shared.open(url)
+        
+    }
+    
+    @IBAction func inAreaButton(_ sender: Any) {
+        let url = URL (string: "https://www.pdxmonthly.com/news-and-city-life/2022/03/how-portland-can-help-ukraine")!
+        UIApplication.shared.open(url)
+    }
+    
+    @IBAction func myLawsButton(_ sender: Any) {
+        let url = URL (string: "https://reproductiverights.org/maps/abortion-laws-by-state/")!
+        UIApplication.shared.open(url)
+    }
+    
+    @IBAction func supportPlannedButtone(_ sender: Any) {
+        let url = URL (string: "https://www.plannedparenthood.org")!
+        UIApplication.shared.open(url)
+    }
+    
+    @IBAction func otherOptionsButton(_ sender: Any) {
+        let url = URL (string: "https://www.wmagazine.com/life/donate-abortion-fund-roe-v-wade-how-to-help")!
+        UIApplication.shared.open(url)
     }
     
     
