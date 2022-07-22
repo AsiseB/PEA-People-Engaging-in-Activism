2529b0c87ec4c5c7a94c8c5674a0ae1579263ce8
kellytrann <kellytran4105@gmail.com>
2022-07-22 03:16:55 -0700

    finished revision

--- PEA/EldersViewController.swift
+++ PEA/EldersViewController.swift
@@ -8,28 +8,48 @@
 import UIKit
 
 class EldersViewController: UIViewController {
+    @IBOutlet weak var SLG: UIButton!
     
-   
+    @IBOutlet weak var MAC: UIButton!
     
-override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+    @IBOutlet weak var SLC: UIButton!
+    
+    @IBOutlet weak var CM: UIButton!
+    
+    @IBOutlet weak var EJ: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()        // Do any additional setup after loading the view.
     }
     
-}
-
-
-
-
-
-
-
-
-
-
-    /*
-    // MARK: - Navigation
+    @IBAction func SLG(_ sender: Any) {
+        let url = URL (string:
+        "https://www.seniorcaucus.org/")!
+        UIApplication.shared.open (url)
+    }
+    
+    @IBAction func MAC(_ sender: Any) {
+        let url = URL (string: "https://norcalresilience.org/12609-2/")!
+        UIApplication.shared.open (url)
+    }
+    
+    @IBAction func SLC(_ sender: Any) {
+        let url = URL (string: "https://eldersaction.org/")!
+        UIApplication.shared.open (url)
+    }
+    
+    @IBAction func CM(_ sender: Any) {
+        let url = URL (string: "https://charitymiles.org/")!
+        UIApplication.shared.open (url)
+    }
+    
+    @IBAction func EJ(_ sender: Any) {
+        let url = URL (string: "http://www.actionforjustice.org/")!
+        UIApplication.shared.open (url)
+    }
+    
+/*
+// MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
@@ -38,5 +58,5 @@ override func viewDidLoad() {
     }
     */
 
-
+}
 
